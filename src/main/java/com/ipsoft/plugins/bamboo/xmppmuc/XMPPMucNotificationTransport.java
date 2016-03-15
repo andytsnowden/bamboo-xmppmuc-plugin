3ac8950916dae5ef3fdfde013ea37382f6e0a630
Andy Snowden <asnowden@ipsoft.com>
2016-03-15 01:31:52 -0400

    removing .idea folder completely, rewrote the XMPP transport to use smack 4.1.6 rather than 3.1.2 that only compiled when the moon was blue.

--- src/main/java/com/ipsoft/plugins/bamboo/xmppmuc/XMPPMucNotificationTransport.java
+++ src/main/java/com/ipsoft/plugins/bamboo/xmppmuc/XMPPMucNotificationTransport.java
@@ -10,16 +10,27 @@ import com.atlassian.bamboo.resultsummary.ResultsSummary;
 import com.atlassian.bamboo.variable.CustomVariableContext;
 import com.atlassian.spring.container.ContainerManager;
 import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jivesoftware.smack.ConnectionConfiguration;
-import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smackx.muc.MultiUserChat;
 
+import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jivesoftware.smack.util.TLSUtils;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.MultiUserChatManager;
+import org.jivesoftware.smack.java7.Java7SmackInitializer;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
+import java.util.List;
 
 public class XMPPMucNotificationTransport implements NotificationTransport
 {
@@ -29,7 +40,7 @@ public class XMPPMucNotificationTransport implements NotificationTransport
     private static final Integer DEFAULT_SSL_PORT = 5223;
     private static final String DEFAULT_RESOURCE = "Bamboo";
     private InstantMessagingServerManager instantMessagingServerManager;
-    private XMPPConnection connection;
+    private XMPPTCPConnection connection;
     private XMPPMucConferenceInstantMessagingServerDefinition imserver;
 
     private final String room;
@@ -78,43 +89,66 @@ public class XMPPMucNotificationTransport implements NotificationTransport
     public void sendNotification(@NotNull Notification notification)
     {
         String message = notification.getIMContent();
-
         XMPPMucConferenceInstantMessagingServerDefinition server = this.getMessagingServerDefinition();
+
+        
         if (server == null) {
             log.error("IM Server is not configured");
-        } else {
-            try {
-                MultiUserChat muc = new MultiUserChat(this.getConnection(server), room);
-                this.muc = muc;
-            } catch (XMPPException e) {
-                log.error("Error creating XMPP MUC Instance");
-                log.trace(e.getStackTrace());
+        } else {            
+
+            if (this.connection != null && this.connection.isConnected()){
+                log.info("Using existing connection");
+            } else {
+                try {
+                    this.connection = this.getConnection(server);
+                } catch (XMPPException e) {
+                    log.info("Unable to get XMPP MUC Connection");
+                    log.info(e.getStackTrace());
+                }
             }
 
-
-            //Add 4 letters to make the name unique (Anti-troll)
+            //Define username and add unique var to prevent duplicate jids
             String code = RandomStringUtils.randomAlphanumeric(4).toUpperCase();
             String DEFAULT_CHANNEL_USERNAME = "Bamboo Test Agent " + code;
 
+            MultiUserChatManager mucm = MultiUserChatManager.getInstanceFor(this.connection);
             try {
-                if (roompw != null && !roompw.isEmpty()) {
-                    this.muc.join(DEFAULT_CHANNEL_USERNAME, this.roompw);
+                List<String> services = mucm.getServiceNames();
+                if (services == null || services.isEmpty()) {
+                    log.info("XMPP MUC no services found, unable to connect to MUC room");
                 } else {
-                    this.muc.join(DEFAULT_CHANNEL_USERNAME);
+                    MultiUserChat muc = mucm.getMultiUserChat(room);
+                    this.muc = muc;
+
+                    try {
+                        if (roompw != null && !roompw.isEmpty()) {
+                            this.muc.join(DEFAULT_CHANNEL_USERNAME, this.roompw);
+                        } else {
+                            this.muc.join(DEFAULT_CHANNEL_USERNAME);
+                        }
+                    } catch (XMPPException e){
+                        log.info("XMPP MUC Exemption while trying to join room.");
+                        log.info(e.getStackTrace());
+                    } catch (SmackException e) {
+                        log.info("XMPP MUC SmackException while trying to join room.");
+                        log.info(e.getStackTrace());
+                    }
+                    this.muc.sendMessage(message);
+                    // Leave the channel when done
+                    this.muc.leave();
                 }
-            } catch (XMPPException e) {
-                log.error("Error joining MUC room: " + this.room);
+            } catch (SmackException.NoResponseException e){
+                log.info("XMPP MUC no response to query for service names");
+                log.trace(e.getStackTrace());
+            } catch (XMPPException.XMPPErrorException e){
+                log.info("XMPP MUC unknown exception");
+                log.trace(e.getStackTrace());
+            } catch (SmackException.NotConnectedException e){
+                log.info("XMPP MUC not connected to query for service names");
                 log.trace(e.getStackTrace());
             }
-            try {
-                this.muc.sendMessage(message);
-            } catch (XMPPException e) {
-                log.error("Error sending message to MUC");
-                log.trace(e.getStackTrace());
-            }
-            // Leave the channel when done
-            this.muc.leave();
         }
+
     }
 
     public XMPPMucConferenceInstantMessagingServerDefinition getMessagingServerDefinition() {
@@ -136,55 +170,85 @@ public class XMPPMucNotificationTransport implements NotificationTransport
      * @throws XMPPException
      * Attempts to return a valid XMPPConnection Object, will create a new connection if one does not already exist.
      */
-    private XMPPConnection getConnection(XMPPMucConferenceInstantMessagingServerDefinition server) throws XMPPException {
-        ConnectionConfiguration configuration;
-        if (this.connection != null) {
-            if (this.connection.isConnected()) {
-                if (server.equals(this.imserver)){
-                    log.info("Using existing XMPP Connection");
-                    return this.connection;
-                } else {
-                    log.info("Not a object or something");
-                }
-            } else {
-                log.info("Not Connected");
+    private XMPPTCPConnection getConnection(XMPPMucConferenceInstantMessagingServerDefinition server) throws XMPPException {
+
+        //Return existing connection if defined/connected
+        if (this.connection != null ) {
+            if (this.connection.isConnected()){
+                log.info("Using existing XMPP Connection");
+                return this.connection;
             }
         } else {
             log.info("Connection is null");
         }
-        //if (this.connection != null && this.connection.isConnected() && server.equals((Object)this.imserver)) {
-        //    log.info("Using existing XMPP Connection");
-        //    return this.connection;
-        //}
         log.info("Creating new XMPP Connection");
 
+        //Pull vars from XMPPMucConferanceIMSDef
         String host = server.getHost();
         String serviceName = "talk.google.com".equals(host) ? "gmail.com" : host;
         Integer port = server.getPort();
         String username = server.getUsername();
         String password = server.getPassword();
-
         log.info("Connecting to " + host + " at " + port + " With user: " + username);
-        ConnectionConfiguration.SecurityMode securityMode = ConnectionConfiguration.SecurityMode.enabled;
-        if (server.isSecureConnectionRequired()) {
+
+        //Connection Builder
+        XMPPTCPConnectionConfiguration.Builder conf = XMPPTCPConnectionConfiguration.builder();
+        conf.setServiceName(serviceName);
+        conf.setHost(host);
+        conf.setPort(port);
+        conf.setUsernameAndPassword(username, password);
+        conf.setCompressionEnabled(true);
+        conf.setConnectTimeout(60);
+
+        //Accept all certs regardless
+        try {
+            TLSUtils.acceptAllCertificates(conf);
+        } catch (NoSuchAlgorithmException e){
+            log.trace(e);
+        } catch (KeyManagementException e){
+            log.trace(e);
+        }
+
+        //Require secure connection
+        if (server.isSecureConnectionRequired()){
             if (port == null) {
                 port = server.isEnforceLegacySsl() ? DEFAULT_SSL_PORT : DEFAULT_TLS_PORT;
             }
-            configuration = new ConnectionConfiguration(host, port.intValue(), serviceName);
-            configuration.setReconnectionAllowed(false);
-            if (server.isEnforceLegacySsl()) {
-                //configuration.setSocketFactory((SocketFactory)new SSLSocketFactoryForBamboo());
-            } else {
-                securityMode = ConnectionConfiguration.SecurityMode.required;
-            }
+            conf.setPort(port);
+            conf.setSecurityMode(SecurityMode.required);
         } else {
-            configuration = port != null ? new ConnectionConfiguration(host, port.intValue(), serviceName) : new ConnectionConfiguration(serviceName);
+            conf.setSecurityMode(SecurityMode.disabled);
         }
-        configuration.setSecurityMode(securityMode);
-        this.imserver = server;
-        this.connection = new XMPPConnection(configuration);
-        this.connection.connect();
-        this.connection.login(server.getUsername(), server.getPassword(), StringUtils.defaultIfEmpty(server.getResource(), "Bamboo"));
+        conf.setHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String arg0, SSLSession arg1) {
+                return true;
+            }
+        });
+
+        //connect
+        new Java7SmackInitializer().initialize();
+        this.connection = new XMPPTCPConnection(conf.build());
+        try {
+            this.connection.connect();
+        } catch (SmackException e) {
+            log.info("XMPP MUC Connection Error");
+            log.info(e.getStackTrace());
+        } catch (IOException e) {
+            log.info("XMPP MUC Connection Error");
+            log.info(e.getStackTrace());
+        }
+
+        //login
+        try {
+            this.connection.login();
+        } catch (SmackException e) {
+            log.info("XMPP MUC Authentication Error");
+            log.info(e.getStackTrace());
+        } catch (IOException e) {
+            log.info("XMPP MUC Connection Error");
+            log.info(e.getStackTrace());
+        }
+
         return this.connection;
     }
 
